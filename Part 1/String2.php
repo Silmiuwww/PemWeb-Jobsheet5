daf6b85f3419ace55a24f393d2180eab6cfb9389
Silmiuww <nadiasalma078@gmail.com>
2025-09-27 21:37:18 +0700

    Soal no 10 Part 1

---
+++ Part 1/String2.php
@@ -0,0 +1,10 @@
+<?php
+echo "Baris baru <br>"; 
+echo "Baris baru <br>"; 
+echo "Halo Dunia <br>"; 
+echo "Halo Dunia <br>"; 
+echo "<pre>Halo Dunia</pre>"; 
+echo "<pre>Halo Dunia!</pre>";
+echo "Katakanlah \"Tidak pada narkoba!\" <br>"; 
+echo 'Katakanlah "Tidak pada narkoba!" <br>'; 
+?>
