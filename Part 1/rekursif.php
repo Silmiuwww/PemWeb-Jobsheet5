b63a0f96a17e5bb5a7dc38ceba37b1e159bc1980
Silmiuww <nadiasalma078@gmail.com>
2025-09-27 21:30:15 +0700

    Soal no 7 dan 8 Part 1

--- Part 1/rekursif.php
+++ Part 1/rekursif.php
@@ -1,11 +1,37 @@
 <?php
-function tampilAngka(int $jumlah, int $indeks) {
-    echo "Angka ke-{$indeks} <br>";
 
-    if ($indeks < $jumlah) {
-        tampilAngka($jumlah, $indeks + 1);
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    ["nama" => "Pantai"],
+                    ["nama" => "Gunung"]
+                ]
+            ],
+            ["nama" => "Kuliner"],
+            ["nama" => "Hiburan"]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
+
+function tampilkanMenuBertingkat(array $menu){
+    echo "<ul>";
+    foreach ($menu as $key => $item){
+        echo "<li>" . $item['nama'] . "</li>";
+        }
+        echo "</ul>";
     }
-}
-
-tampilAngka(20, 1);
+tampilkanMenuBertingkat($menu);
 ?>
