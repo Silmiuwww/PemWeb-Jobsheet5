683e204d13ec19e11d558bc8bda30595b1a4ca79
Silmiuww <nadiasalma078@gmail.com>
2025-09-30 17:43:17 +0700

    Soal no 18 Part 2

---
+++ Part 2/entities.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Entities HTML</title>
+</head>
+<body>
+    <p>It&#39;s time to read a HTML5 book.</p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;keuntungan dari menggunakan nama entities: <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Sebuah nama entities mudah diingat. Kerugian dari menggunakan nama <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;entities: Browser mungkin tidak mendukung semua nama entities, tetapi <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;dukungan untuk nomor lebih baik.</p>
+    <p>&#169; 2023 jti.com</p>
+</body>
+</html>
